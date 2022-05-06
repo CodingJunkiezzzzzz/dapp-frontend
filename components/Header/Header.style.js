ec4bca52abede96255a279f1b398e120e7c6b51e
anie308 <aniebietabasiobo@gmail.com>
2022-05-06 10:23:58 +0100

    navbar

---
+++ components/Header/Header.style.js
@@ -0,0 +1,104 @@
+import styled from "styled-components";
+
+// type DivProps= {
+//     click : any
+//  }
+
+export const HeaderWrapper = styled.div`
+  width: 100%;
+  background: linear-gradient(90.02deg, #000000 5.44%, #161525 54.52%);
+`;
+export const Nav = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 10px 30px;
+  justify-content: space-between;
+
+ 
+
+  @media screen and (max-width: 900px) {
+    padding: 0;
+    flex-direction:column ;
+  
+  }
+`;
+export const Logo = styled.div``;
+
+export const MobileWrapper = styled.div`
+
+    display: flex;
+    align-items: center;
+
+    @media screen and (max-width: 900px) {
+        padding:10px ;
+      width: 100%;
+      justify-content:space-between ;
+    }
+
+
+`;
+
+export const ButtonCon = styled.div`
+    display:none ;
+    align-items:center ;
+    justify-content:center ;
+
+  .icon {
+    color: #ffff;
+    font-size: 25px;
+    cursor: pointer;
+  }
+
+  @media screen and (max-width:900px){
+      display:flex ;
+      
+  }
+`;
+export const NavLinks = styled.div`
+padding:0 30px ;
+flex-grow:2;
+color:#fff ;
+
+
+@media screen and (max-width:900px){
+    padding:0px ;
+    left: ${({click}) => (click? 0: '-100%')};
+    position: fixed ;
+    top:0 ;
+    background:#fff ;
+    width:250px ;
+    height:300px ;
+}
+`;
+
+export const Div = styled.div`
+padding:0 20px ;
+display:flex ;
+align-items:center ;
+justify-content: space-between;
+
+`
+export const SubDiv = styled.div`
+display:flex ;
+align-items:center ;
+
+`
+export const NavLink= styled.div`
+color:#ffff ;
+margin: 0 20px;
+`
+
+export const WalletBtn = styled.div` 
+display:flex ;
+align-items:center ;
+background:#1673B9 ;
+padding:10px 20px ;
+border-radius:20px ;
+cursor: pointer;
+font-size:18px ;
+
+.icon{
+    color: #ffff;
+    font-size: 18px;
+}
+`
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,8 +1,66 @@
+import { Container } from "../../styles/GlobalStyle";
+import {
+  Nav,
+  Logo,
+  ButtonCon,
+  MobileWrapper,
+  HeaderWrapper,
+  NavLinks,
+  Div,
+  SubDiv,
+  NavLink,
+  WalletBtn
+} from './Header.style'
+import LogoImg from '../../assets/logo.png'
+import Image from "next/image";
+import {FaBars} from 'react-icons/fa'
+import {BiMouse} from 'react-icons/bi'
+import Link from "next/link";
+
+
+
 const Header = () => {
+
+
+
   return (
-    <>
-      <h1>Header Page</h1>
-    </>
+    <HeaderWrapper>
+    <Container>
+      <Nav>
+        <MobileWrapper>
+        <Logo> <Image src={LogoImg} alt="vefidex logo" /></Logo>
+        <ButtonCon>
+              <FaBars className="icon"/>
+        </ButtonCon>
+        </MobileWrapper>
+        <NavLinks>
+          <Div>
+            <SubDiv>
+              <NavLink>
+                <Link href='/'><a>Swap</a></Link>
+                </NavLink>
+              <NavLink>
+                <Link href='/'><a>Bridge</a></Link>
+                </NavLink>
+              <NavLink>
+                <Link href='/'><a>Lend</a></Link>
+                </NavLink>
+              <NavLink>
+                <Link href='/'><a>LaunchPad</a></Link>
+                </NavLink>
+            </SubDiv>
+            <SubDiv>
+              <WalletBtn>
+                <BiMouse className="icon"/>
+                Connect Wallet
+              </WalletBtn>
+              </SubDiv>
+          </Div>
+        </NavLinks>
+        
+      </Nav>
+    </Container>
+    </HeaderWrapper>
   );
 };
 
