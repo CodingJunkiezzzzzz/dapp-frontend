09820545114074ea3e5efcbcacd8cc42291f5879
Kingsley Victor <javaprodigy56@gmail.com>
2022-10-12 23:25:14 +0100

    add web3 connectors

---
+++ api/dex.ts
@@ -0,0 +1,21 @@
+import { hexValue } from '@ethersproject/bytes';
+import { default as rootDAppClient } from './root';
+import { ListingModel, SyncModel } from './models/dex';
+
+export const fetchListing = (chainId: number) => {
+  return new Promise<Array<ListingModel>>((resolve, reject) => {
+    rootDAppClient
+      .get(`/dex/listing/${chainId}`)
+      .then((res) => resolve(res.data.result))
+      .catch(reject);
+  });
+};
+
+export const fetchPriceHistoryForPair = (pair: string, chainId: number, period?: number) => {
+  return new Promise<Array<SyncModel>>((resolve, reject) => {
+    rootDAppClient
+      .get(`/dex/price_history/${pair}/${hexValue(chainId)}${period ?? `?period=${period}`}`)
+      .then((res) => resolve(res.data.result))
+      .catch(reject);
+  });
+};
---
+++ contexts/api/index.tsx
@@ -0,0 +1,3 @@
+import React, { createContext, useContext, useState, useEffect } from 'react';
+
+type APIContextType = {};
