bda7f10c46057e0673a3ebfa3a2fadcc748d3832
Kingsley Victor <javaprodigy56@gmail.com>
2022-10-13 02:58:14 +0100

    ...

--- contexts/api/index.tsx
+++ contexts/api/index.tsx
@@ -1,3 +1,25 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
+import { ListingModel } from '../../api/models/dex';
+import { useWeb3Context } from '../web3';
+import { fetchListing } from '../../api/dex';
 
-type APIContextType = {};
+type APIContextType = {
+  tokensListing: Array<ListingModel>;
+};
+
+const APIContext = createContext({} as APIContextType);
+
+export const APIContextProvider = ({ children }: any) => {
+  const { chainId } = useWeb3Context();
+  const [tokensListing, setTokensListing] = useState<Array<ListingModel>>([]);
+
+  useEffect(() => {
+    fetchListing(chainId || 97)
+      .then(setTokensListing)
+      .catch(console.log);
+  }, [chainId]);
+
+  return <APIContext.Provider value={{ tokensListing }}>{children}</APIContext.Provider>;
+};
+
+export const useAPIContext = () => useContext(APIContext);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { DEXSettingsContextProvider } from '../contexts/dex/settings';
 import { Web3ContextProvider } from '../contexts/web3';
+import { APIContextProvider } from '../contexts/api';
 
 function getLibrary(provider: any) {
   return new Web3(provider);
@@ -17,13 +18,15 @@ export default function App({ Component, pageProps }: AppProps) {
       <Web3ReactProvider getLibrary={getLibrary}>
         <Web3ContextProvider>
           <DEXSettingsContextProvider>
-            <div className="bg-[url('/images/bg.svg')] bg-no-repeat bg-cover h-screen scroll-smooth flex flex-col w-screen overflow-auto">
-              <Header />
-              <div className="overflow-auto flex-1 backdrop-opacity-10 backdrop-invert bg-[#000]/70">
-                <Component {...pageProps} />
+            <APIContextProvider>
+              <div className="bg-[url('/images/bg.svg')] bg-no-repeat bg-cover h-screen scroll-smooth flex flex-col w-screen overflow-auto">
+                <Header />
+                <div className="overflow-auto flex-1 backdrop-opacity-10 backdrop-invert bg-[#000]/70">
+                  <Component {...pageProps} />
+                </div>
+                <Footer />
               </div>
-              <Footer />
-            </div>
+            </APIContextProvider>
           </DEXSettingsContextProvider>
         </Web3ContextProvider>
       </Web3ReactProvider>
